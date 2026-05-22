ff737009abeffd90f1c5fdebae5c8afcd07efe66
Farouk Bello <faroukk.bello@gmail.com>
2026-05-22 08:37:23 +0100

    Add deployment workflow for JubalBoard Web

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "i.pravatar.cc" },
+      { protocol: "https", hostname: "api.jubalboard.com" },
     ],
   },
 };
